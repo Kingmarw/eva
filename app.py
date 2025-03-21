84bf8995f287d0097e73f4f152216c4698e8a446
Kingmarw <163139467+Kingmarw@users.noreply.github.com>
2025-03-21 22:59:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -230,4 +230,4 @@ def logout():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, hostname="localhost", port=0.0.0.0)
+    app.run(debug=True, hostname="localhost", port="0.0.0.0")
